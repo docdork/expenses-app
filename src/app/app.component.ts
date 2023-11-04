6ae97947143844e78a7c898966c44943a44ea97a
docdork <97050042+docdork@users.noreply.github.com>
2023-11-04 12:54:06 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
+    { title: 'Create Expense', url: '/folder/create', icon: 'keypad' },
+    { title: 'Expenses List', url: '/folder/list', icon: 'reader' },
     { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
     { title: 'Archived', url: '/folder/archived', icon: 'archive' },
     { title: 'Trash', url: '/folder/trash', icon: 'trash' },
